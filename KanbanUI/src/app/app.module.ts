4c5a003bdf32b85a0739eb3bae215ecbda528dc8
Nitya Prakash Sharma <nityaprakash@gmail.com>
2017-08-30 08:09:17 -0400

    Added Dashboard component

--- KanbanUI/src/app/app.module.ts
+++ KanbanUI/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContainerComponent } from './container/container.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
-    ContainerComponent
+    ContainerComponent,
+    DashBoardComponent
   ],
   imports: [
     BrowserModule
