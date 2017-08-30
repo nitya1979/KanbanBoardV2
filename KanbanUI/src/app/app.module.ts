a08f19de0bdcf7e24469db927e7b917642e4a7e5
Nitya Prakash Sharma <nityaprakash@gmail.com>
2017-08-29 22:01:34 -0400

    Added Header/Footer and content

--- KanbanUI/src/app/app.module.ts
+++ KanbanUI/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule
