7648b24b6af7d49db47603eb3b306d0bf4b83f4f
Nitya Prakash Sharma <nityaprakash@gmail.com>
2017-09-05 06:54:06 -0400

    fixed ui

--- KanbanUI/src/app/task-list/task-list.component.ts
+++ KanbanUI/src/app/task-list/task-list.component.ts
@@ -9,6 +9,7 @@ export class TaskListComponent implements OnInit {
   tasks:any;
   columns:any =[
     {caption : "Task Summary", name:"Summary"},
+    {caption : "Priority", name:"Priority"},
     {caption : "Project", name:"Project"},
     {caption : "Due Date", name:"DueDate"}
   ]
@@ -19,18 +20,22 @@ export class TaskListComponent implements OnInit {
     this.tasks = [
         {
             Summary: "Task 1 For Project 1",
+            Priority: "Critical",
             Project: "Project 1",
             DueDate: "09/30/2017",
         },{
             Summary: "Task 2 For Project 1",
+            Priority: "Major",
             Project: "Project 1",
             DueDate: "09/30/2017",
         },{
             Summary: "Task 1 For Project 2",
+            Priority: "Medium",
             Project: "Project 2",
             DueDate: "09/30/2017",
         },{
             Summary: "Task 2 For Project 2",
+            Priority: "Low",
             Project: "Project 2",
             DueDate: "09/30/2017",
         }
